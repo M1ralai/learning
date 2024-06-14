0495fb6a9468fd08aceca36c4afc6bbb59290114
M1ralai <atahan.dmrr@gmail.com>
2024-06-14 14:31:41 +0300

    Add files via upload

--- square.go
+++ square.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	var number1, number2, listing, number, number_square int
+	var number1, number2, number3, listing, number, number_square int
 	var eternity bool = false
 	for eternity == false {
 		listing = 1
@@ -13,14 +13,9 @@ func main() {
 		fmt.Scanln(&number1, &number2)
 		switch {
 		case number1 < number2:
-			number = number1
-			for number <= number2 {
-				number_square = number * number
-				fmt.Println(listing, ". number is", number, "and its square is ", number_square)
-				number = number + 1
-				listing = listing + 1
-			}
-			break
+			number1 = number3
+			number2 = number1
+			number2 = number3
 		case number2 < number1:
 			number = number2
 			for number <= number1 {
